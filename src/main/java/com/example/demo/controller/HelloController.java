152d7a616f592230eb9efe350671380c9bb8b8f6
wrxa <wr.xa@163.com>
2018-07-10 22:55:43 +0800

    update

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+
+package com.example.demo.controller;
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HelloController {
+ 
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
+ 
